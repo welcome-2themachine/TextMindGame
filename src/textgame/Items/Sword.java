7c138f50a932d582deeb78227d5e81a41945e83b
welcome-2themachine <Christopher.prevost94@gmail.com>
2017-01-07 15:47:54 -0700

    Test
    TEST

--- src/textgame/Items/Sword.java
+++ src/textgame/Items/Sword.java
@@ -8,7 +8,14 @@ package textgame.Items;
  * @author chris
  */
 public class Sword extends Weapon{
+    protected double specialAttack;
+    
     public Sword(){
-        
+        specialAttack = 0;
     }
+    
+    public Sword (int spcAtk){
+        this.specialAttack = spcAtk;
+    } 
+    
 }
